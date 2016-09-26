4f0631448aa00f07fd081962cc29a41cdb181f11
Nikhil Bhaskar <nikhil38@gmail.com>
2016-09-25 20:22:39 -0400

    created util module

--- work/initialize.js
+++ work/initialize.js
@@ -4,26 +4,26 @@ let Queue = require('../work/Queue'),
 
 /*
  * Initialize all data
-*/
+ */
 const initialize = () => {
 
-  /*
-   * The key represents the seating capacity in terms of # of people
-   * The value represents the quantity of the table
-  */
-	const arrivalNumbersDaily = {
-  		1: 5,
-  		2: 20,
-  		3: 20,
-  		4: 15,
-  		5: 10
-	}
+    /*
+     * The key represents the seating capacity in terms of # of people
+     * The value represents the quantity of the table
+     */
+    const arrivalNumbersDaily = {
+        1: 5,
+        2: 20,
+        3: 20,
+        4: 15,
+        5: 10
+    }
 
-  /*
-   * The key represents the number of people in a group who enter a restaurant
-   * The value represents the number of times a day a group of size [key] enters
-  */
-	const tablesAvailableTemplate = new Map([
+    /*
+     * The key represents the number of people in a group who enter a restaurant
+     * The value represents the number of times a day a group of size [key] enters
+     */
+    const tablesAvailableTemplate = new Map([
         [2, 4],
         [3, 3],
         [4, 2],
@@ -38,26 +38,26 @@ const initialize = () => {
     let tablesAvailable = new TablesAvailable(tablesAvailableTemplate);
 
     /*
-    * a queue representing the people who are waiting to enter
-    */
-	let enterQueue = new Queue();
+     * a queue representing the people who are waiting to enter
+     */
+    let enterQueue = new Queue();
 
     /*
-    * a queue representing the people who are eating, ready to eventually exit
-    */
-	let exitQueue = new Queue();
-	let revenue = 0;
+     * a queue representing the people who are eating, ready to eventually exit
+     */
+    let exitQueue = new Queue();
+    let revenue = 0;
 
-	return {
-		totalArrivalsDaily,
-		likelyHoods,
-		arrivals,
-		shuffledArrivals,
-		tablesAvailable,
-		enterQueue,
-		exitQueue,
-		revenue
-	}
+    return {
+        totalArrivalsDaily,
+        likelyHoods,
+        arrivals,
+        shuffledArrivals,
+        tablesAvailable,
+        enterQueue,
+        exitQueue,
+        revenue
+    }
 }
 
 module.exports = initialize;
